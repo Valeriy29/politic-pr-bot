1fb01fc97d4e20374697e97fa2c4db81a06c2642
Valeriy29 <walyas89@gmail.com>
2019-11-25 00:03:17 +0200

    dev part 2 heroku

--- src/main/java/com/example/constant/Admin.java
+++ src/main/java/com/example/constant/Admin.java
@@ -4,10 +4,10 @@ public enum Admin {
 
     ADMIN_TELEGRAM_ID("370678219"),
     BASIC_URL("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s"),
-//    TOKEN("842868136:AAE3lu7F7gxNBCa_5OyuHmuIzvOYTFlKrRo"),
-//    BOT_NAME("politic_pr_bot")
-    TOKEN("920411958:AAEjN4VyKGZYkoKZalvdIfhD0PROnFGtwL0"),
-    BOT_NAME("dating_zp_bot"),
+    TOKEN("842868136:AAE3lu7F7gxNBCa_5OyuHmuIzvOYTFlKrRo"),
+    BOT_NAME("politic_pr_bot"),
+//    TOKEN("920411958:AAEjN4VyKGZYkoKZalvdIfhD0PROnFGtwL0"),
+//    BOT_NAME("dating_zp_bot"),
     EMAIL("info@rccompany.ru");
 
     private String constant;
